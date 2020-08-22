74b702e0b414a9c78582fd7c0c1b845bb6822dc1
DnyaneshD <dnyan.desh@gmail.com>
2020-08-22 19:23:06 +0200

    Test deployment

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+const app = express();
+const port = 5000;
+
+console.log("The application is running on ", port);
+
+app.get("/ping", async (req, res) => {
+  res.send("Test passed");
+});
+
+// start the Express server
+app.listen(port);
