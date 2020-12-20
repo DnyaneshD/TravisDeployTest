8f287b5ecbbf77f3495a069093c313f2980b1276
DnyaneshD <dnyan.desh@gmail.com>
2020-12-20 12:34:08 +0100

    test push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 const app = express();
 const port = 5000;
 
-console.log("The application is running on ", port);
+console.log("The application is running on:", port);
 
 app.get("/ping", async (req, res) => {
   res.send("Test passed");
